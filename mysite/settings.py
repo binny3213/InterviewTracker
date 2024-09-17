e432629e27cca2e2761eed34ff6ec87b4fcaa7bb
binny <90454079+binny3213@users.noreply.github.com>
2024-09-17 18:50:24 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,11 +14,6 @@ from pathlib import Path
 import os
 
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
-application = get_wsgi_application()
 
 
 
