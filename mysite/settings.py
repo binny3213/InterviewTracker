7903eaa280966c2390c89261c4a0b3de2722f158
binny <90454079+binny3213@users.noreply.github.com>
2024-09-19 11:09:53 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -137,7 +137,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,'interview/static'),)
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-
+import django_heroku
+django_heroku.settings(locals())
 
 
 # Default primary key field type
